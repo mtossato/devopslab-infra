1171e0a252f8a0b79e9c222a95c3c44c47f2b8f4
mtossato <72095990+mtossato@users.noreply.github.com>
2022-03-22 22:27:24 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,24 +1,13 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "firstvm" {
-  name         = "helloworld"
-  machine_type = "n1-standard-1"
-  zone         = "us-west1-b"
-  	
-  # Defini a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20220308"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
-  }
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-cloud-tossato"
+  location_id = "us-central"
 }
 
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
 
+  location = "us-central1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
--- providers.tf
+++ providers.tf
@@ -10,12 +10,15 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
- 
-
-
   project = "lab-devops-cloud-tossato"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
 
+provider "google-beta" {
+
+  project = "lab-devops-cloud-tossato"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
 
