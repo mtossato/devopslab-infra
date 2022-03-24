5ddaa606bc42a6a91525c8ad963e68ef74241d19
mtossato <72095990+mtossato@users.noreply.github.com>
2022-03-23 23:50:01 -0300

    teste

---
+++ gcp-main.tf
@@ -0,0 +1,13 @@
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-cloud2"
+  location_id = "us-west"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+
+  location = "us-west1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
